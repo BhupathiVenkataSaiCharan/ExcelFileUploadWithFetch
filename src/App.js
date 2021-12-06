2391beefab727bd8578ab9877157afe204db0ad3
BhupathiVenkataSaiCharan <77479689+BhupathiVenkataSaiCharan@users.noreply.github.com>
2021-12-06 18:29:11 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+        constructor(props) {
+		      super(props);
+		      this.state = {
+            file: '', 
+            msg: ''
+          };
+	      }
+
+        onFileChange = (event) => {
+          this.setState({
+            file: event.target.files[0]
+          });
+        }
+        
+        uploadFileData = (event) => {
+          event.preventDefault();
+          this.setState({msg: ''});
+      
+          let data = new FormData();
+          data.append('file', this.state.file);
+      
+          fetch('http://localhost:8080/upload', {
+            method: 'POST',
+            body: data
+          }).then(response => {
+            this.setState({msg: "File successfully uploaded"});
+          }).catch(err => {
+            this.setState({error: err});
+          });
+      
+        }
+
+  render(){
+    return(
+      <>
+          <div id="container">
+              <h1>File Upload Example using React</h1>
+              <h3>Upload a File</h3>
+              <h4>{this.state.msg}</h4>
+
+              <input onChange={this.onFileChange} 
+                      type="file" 
+                      accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"/>
+
+              <button disabled={!this.state.file} onClick={this.uploadFileData}>Upload</button>
+			    </div>
+      </>
+    );
+    }
 }
+  
 
 export default App;
